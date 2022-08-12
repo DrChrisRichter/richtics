4528a594d960804499ba9098a195d059f65a513d
Chris Richter <mr.chris.richter@gmail.com>
2022-08-12 20:34:19 +0200

    fixed icons and href

--- form-handler.php
+++ form-handler.php
@@ -4,18 +4,19 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$email_from = 'chris@richtics.com'
-$email_subject = 'new for submission'
-$email_body = 'user name: $name.\n'.
-              'user email: $visitor_email.\n'.
-              'subject: $subject.\n'.
-              'message: $message.\n';
+$email_from = 'chris@richtics.com';
+$email_subject = 'new for submission';
 
-$to = 'mr.chris.richter@gmail.com'
+$email_body = "user name: $name.\n".
+              "user email: $visitor_email.\n".
+              "subject: $subject.\n".
+              "message: $message.\n";
+
+$to = 'mr.chris.richter@gmail.com';
 $headers = "From: $email_from \r\n";
 $headers .= "Reply-To: $visitor_email \r\n";
 
-mail($to, $email_subject, @email_body, $headers)
+mail($to, $email_subject, $email_body, $headers);
 
-header("Location: contact.html")
+header("Location: contact.html");
 ?>
